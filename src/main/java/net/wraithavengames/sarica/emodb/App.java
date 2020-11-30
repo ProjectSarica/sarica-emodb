61aed218bbec5ed379a447051d049749f9a5b6e2
TheDudeFromCI <thedudefromci@gmail.com>
2020-11-29 18:29:10 -0700

    Created project

---
+++ src/main/java/net/wraithavengames/sarica/emodb/App.java
@@ -0,0 +1,13 @@
+package net.wraithavengames.sarica.emodb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/net/wraithavengames/sarica/emodb/AppTest.java
@@ -0,0 +1,20 @@
+package net.wraithavengames.sarica.emodb;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
