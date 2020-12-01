66004fa99dff6adb02a1a1d96b71aba7d1a27860
TheDudeFromCI <thedudefromci@gmail.com>
2020-11-30 21:58:12 -0700

    Updated base project structure.

--- src/main/java/net/wraithavengames/sarica/emodb/App.java
+++ src/main/java/net/wraithavengames/sarica/emodb/App.java
@@ -1,13 +1,16 @@
 package net.wraithavengames.sarica.emodb;
 
+import org.apache.log4j.Logger;
+
 /**
  * Hello world!
- *
  */
 public class App 
 {
+    private static final Logger log = Logger.getLogger(App.class);
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        log.info("Starting Emotional Database...");
     }
 }
--- src/test/java/net/wraithavengames/sarica/emodb/AppTest.java
+++ src/test/java/net/wraithavengames/sarica/emodb/AppTest.java
@@ -1,20 +1,20 @@
 package net.wraithavengames.sarica.emodb;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
+    
     /**
      * Rigorous Test :-)
      */
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        assertTrue(true);
     }
 }
